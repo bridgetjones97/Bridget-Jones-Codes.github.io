1cfc970d50ad7157761a54bc15dc5f116412442d
bridgetjones97 <143801638+bridgetjones97@users.noreply.github.com>
2023-09-23 16:13:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"; 
+    document.body.style.backgroundImage = "none";
+    document.getElementById("#").style.color = "white";
+}
